b544fa49496de4d67681d7fb157b150b33f28b0b
Dharmveer Singh <77281352+Dharmveer47@users.noreply.github.com>
2021-12-22 05:35:17 -0800

    Adding new project

--- src/components/Obj.js
+++ src/components/Obj.js
@@ -12,6 +12,17 @@ const ProjectsInfo = [
     },
     {
         "id": "02",
+        "Name": "Service Provider",
+        "Info": `This is simple UI   .`,
+        "link": "https://insignia47.netlify.app/",
+        "gitLink": "https://github.com/Dharmveer47/insigni",
+        "skill": ['HTML5','CSS3','React Js'],
+        "img": "https://raw.githubusercontent.com/Dharmveer47/PortfolioImg/master/Insignia.png",
+        // "figma": "https://www.figma.com/file/JN0aLCqm5CifsX3AZ532f6/Portfolio",
+        "figma": "https://www.figma.com/file/WK9IJLbqIrpt79HQYpUBLL/Frontend-developer-Assignment-(Copy)?node-id=413%3A3"
+    },
+    {
+        "id": "03",
         "Name": "Weather Info",
         "Info": "It's simple weather web. Here's you search your favorite city to know their status. It's also fully responsive . I can exp some fetured such as dynamic background image update and so more...",
         "link": "https://weather47.netlify.app",
@@ -23,7 +34,7 @@ const ProjectsInfo = [
         "figma": "https://weather47.netlify.app"
     },
     {
-        "id": "03",
+        "id": "04",
         "Name": "Food Landing page ",
         "Info": "It's beautiful food Landing page as you can see in pic. fully responsive , ",
         "link": "https://codepen.io/dharmveer47/pen/rNmzgQy",
